d2554afcdf364a819bc6e59f562a09e9a81141a2
Doug McBride <doug.mcbride@disney.com>
2021-08-09 12:09:36 -0700

    Try compiling an empty source file to work around dependency issue

--- Package.swift
+++ Package.swift
@@ -18,6 +18,7 @@ let package = Package(
             name: "KochavaTracker",
             targets:
             [
+                "DummySource",
                 "KochavaTracker"
             ]
         ),
@@ -36,6 +37,7 @@ let package = Package(
     [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(name: "DummySource", dependencies: [], path: "Sources"),
         .binaryTarget(
             name: "KochavaTracker",
             path: "KochavaTracker.xcframework"
--- Sources/KochavaTracker/KochavaTracker.swift
+++ Sources/KochavaTracker/KochavaTracker.swift
@@ -1,4 +1,4 @@
 
-struct KochavaTracker
+struct Dummy
 {
 }
